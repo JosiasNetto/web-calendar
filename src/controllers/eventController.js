d7eedd7d327e4e1b26ab45811588a95eff5122bd
JosiasNetto <josiascaldasnetto08@gmail.com>
2025-03-11 13:34:22 -0300

    rota deletar evento

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -20,6 +20,33 @@ const EventController = {
         }catch(error){
             res.status(500).json({mensagem: "Erro ao criar evento", erro: error.message });
         }
+    },
+
+    async deleteEvent(req, res) {
+        const userId = req.params.userId;
+        const eventId = req.params.eventId;
+
+        try {
+            const userEvent = await User.findById(userId);
+            if(!userEvent){
+                throw new Error("Usuario não encontrado")
+            }
+            console.log("Passou do user", userId)
+
+            const event = await Event.findById(eventId);
+            if(!event){
+                throw new Error("Evento não encontrado")
+            }
+
+            await Event.findByIdAndDelete(eventId);
+
+            userEvent.eventos = userEvent.eventos.filter(id => id.toString() !== eventId);
+            await userEvent.save();
+
+            res.status(200).json({mensagem: "Evento deletado com sucesso"});
+        }catch(error){
+            res.status(500).json({mensagem: "Erro ao deletar evento", erro: error.message })
+        }
     }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,13 @@ import express from "express";
 
 function routes(app){
     app.use(express.json()); 
+    
+    app.delete("/:userId/:eventId", EventController.deleteEvent);
 
     app.post("/register", UserController.registerUser);
     app.post("/login", UserController.loginUser);
-    app.post("/:userId/evento", EventController.registerEvent)
+    app.post("/:userId/evento", EventController.registerEvent);
+
 
 }
 
